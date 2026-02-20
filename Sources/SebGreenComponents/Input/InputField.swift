ee580493a3015c97a51670b2beb8fbed905a6edc
Jacob Jidell <jacob.jidell@seb.se>
2026-02-20 10:52:05 +0100

    Created validation support for InputField
    Adjusted horizontal padding for validationView

--- Sources/SebGreenComponents/Input/InputField.swift
+++ Sources/SebGreenComponents/Input/InputField.swift
@@ -8,73 +8,52 @@ struct InfoButton: View {
     }
 }
 
-extension EnvironmentValues {
-    @Entry var inputFieldStyle: InputFieldStyle = .default
-    @Entry var textInputCharacterLimit: Int?
-    @Entry var optionalInput: Bool = false
-    @Entry var clearButtonEnabled = false
-    @Entry var supportiveText: String?
-}
-
-extension View {
-    func inputFieldStyle(_ style: InputFieldStyle) -> some View {
-        environment(\.inputFieldStyle, style)
-    }
-    
-    func textInputCharacterLimit(_ limit: Int) -> some View {
-        environment(\.textInputCharacterLimit, limit)
-    }
-    
-    func optionalTextInput() -> some View {
-        environment(\.optionalInput, true)
-    }
-    
-    func clearButtonEnabled() -> some View {
-        environment(\.clearButtonEnabled, true)
-    }
-    
-    func supportiveText(_ text: String) -> some View {
-        environment(\.supportiveText, text)
-    }
-}
-
 enum InputFieldStyle {
     case `default`
     case floating
 }
 
-@resultBuilder
-public struct AccessoryBuilder {
-    public static func buildBlock() -> EmptyView {
-        EmptyView()
-    }
-
-    static func buildBlock(_ component: InfoButton) -> some View {
-        component
-    }
-}
-
 struct InputField<Accessory: View>: View {
     @Environment(\.inputFieldStyle) private var inputStyle
-    @Environment(\.optionalInput) private var isOptionalInput
+    @Environment(\.optionalField) private var optionalField
+    @Environment(\.validationError) private var validationError
 
     @State private var text: String = ""  // TODO: Setup a binding text to the parent or just change this to binding depending on requirement
     private let label: any StringProtocol
     private let accessory: Accessory
-    
+
     private var title: any StringProtocol {
-        isOptionalInput ? label.appending(" (optional)") : label
+        guard optionalField else { return label }
+        let optional = String(localized: "optional")
+        return "\(label) (\(optional))"
+    }
+
+    private var hasValidationError: Bool {
+        validationError != nil
     }
 
     init(
         _ label: any StringProtocol,
-        @AccessoryBuilder accessory: () -> Accessory = AccessoryBuilder.buildBlock
+        @AccessoryBuilder accessory: () -> Accessory = AccessoryBuilder
+            .buildBlock
     ) {
         self.label = label
         self.accessory = accessory()
     }
 
     var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            inputField
+
+            if let validationError {
+                validationView(validationError)
+            }
+        }
+        .animation(.default, value: hasValidationError)
+    }
+    
+    @ViewBuilder
+    private var inputField: some View {
         switch inputStyle {
         case .default:
             DefaultLabel(
@@ -103,34 +82,59 @@ struct InputField<Accessory: View>: View {
         }
         .foregroundStyle(Color.contentNeutral01)
     }
+    
+    private func validationView(_ error: Error) -> some View {
+        HStack {
+            Image(systemName: "exclamationmark.square.fill")
+                .accessibilityHidden(true)
+            Text(error.localizedDescription)
+                .typography(.bodyBookS)
+                .fixedSize(horizontal: false, vertical: true)
+
+        }
+        .foregroundColor(Color.contentNegative01)
+        .padding(.horizontal, 14) // Needs to be 14px due to 2px border.
+    }
 }
 
+
+@available(iOS 17, *)
 #Preview {
+    @Previewable @State var hasError = false
+
     ScrollView {
         ZStack {
             RoundedRectangle(cornerRadius: 16)
                 .foregroundStyle(Color.l1Neutral02)
-            
+
             VStack(spacing: 24) {
+                Toggle("Toggle error", isOn: $hasError)
+
+                Divider()
+
                 InputField("Custom header") {
                     InfoButton {
-                        
+
                     }
                 }
                 .inputFieldStyle(.default)
                 .supportiveText("Hello")
-                .optionalTextInput()
-                
+                .optionalField()
+                .validation(
+                    hasError ? NSError(domain: "", code: 999) : nil
+                )
+
                 Divider()
-                
-                InputField("Floating header 2") {
-                    InfoButton {
-                        
-                    }
-                }
-                .inputFieldStyle(.floating)
+
+                InputField("Floating header 2")
+                    .inputFieldStyle(.floating)
+                    .clearable()
+                    .validation(
+                        hasError ? NSError(domain: "", code: 999) : nil
+                    )
             }
             .padding()
         }
+        .animation(.default, value: hasError)
     }
 }
