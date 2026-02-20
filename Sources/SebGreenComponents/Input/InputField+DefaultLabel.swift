a0810aa370545dc2b9483e8ddab6cca01cb5cd03
Jacob Jidell <jacob.jidell@seb.se>
2026-02-20 09:55:43 +0100

    Added accessory, supportiveText and adjusted default design

---
+++ Sources/SebGreenComponents/Input/InputField+DefaultLabel.swift
@@ -0,0 +1,85 @@
+import SwiftUI
+
+extension InputField {
+    struct DefaultLabel<TextField: View>: View {
+        @Environment(\.verticalSizeClass) private var verticalSizeClass
+        @Environment(\.supportiveText) private var supportiveText
+
+        @FocusState private var isFocused: Bool
+        @Binding var text: String
+
+        private let label: any StringProtocol
+        private let textField: TextField
+        private let accessory: Accessory
+
+        init(
+            _ label: any StringProtocol,
+            text: Binding<String>,
+            textField: TextField,
+            accessory: Accessory
+        ) {
+            self._text = text
+            self.label = label
+            self.textField = textField
+            self.accessory = accessory
+        }
+
+        private var minimumFrameHeight: CGFloat {
+            verticalSizeClass == .compact ? 54 : 64
+        }
+
+        var body: some View {
+            VStack(alignment: .leading, spacing: 8) {
+                header
+                textField
+                    .focused($isFocused)
+                    .padding(.horizontal, 16)
+                    .frame(minHeight: minimumFrameHeight)
+                    .background {
+                        RoundedRectangle(cornerRadius: 16)
+                            .fill(Color.l2Neutral02)
+                    }
+                    .background {
+                        RoundedRectangle(cornerRadius: 16)
+                            .stroke(style: .init(lineWidth: 1))
+                    }
+                    .contentShape(.rect(cornerRadius: 16))
+                    .onTapGesture {
+                        isFocused = true
+                    }
+            }
+        }
+
+        private var header: some View {
+            VStack(alignment: .leading, spacing: 0) {
+                HStack {
+                    Text(label)
+                        .typography(.bodyBookM)
+                        .foregroundStyle(Color.contentNeutral01)
+
+                    Spacer()
+
+                    accessory
+                }
+                if let supportiveText {
+                    Text(supportiveText)
+                        .typography(.bodyBookS)
+                        .foregroundStyle(Color.contentNeutral02)
+                        .padding(.trailing, 16)
+                }
+            }
+            .padding(.leading, 16)
+        }
+    }
+}
+
+#Preview {
+    InputField("Floating")
+        .inputFieldStyle(.default)
+        .padding()
+        .background {
+            RoundedRectangle(cornerRadius: 16)
+                .foregroundStyle(Color.l1Neutral02)
+        }
+        .padding(.horizontal)
+}
