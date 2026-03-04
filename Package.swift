7c9596e458b8d6756c18e1b523132addee53a13e
Herman Olsson <herman.olsson@framna.com>
2026-03-04 09:04:46 +0100

    Update to 0.0.7

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/seb-oss/green-tokens-ios",
-            exact: "0.0.6"
+            exact: "0.0.7"
         ),
         .package(
             url: "https://github.com/pointfreeco/swift-snapshot-testing",
