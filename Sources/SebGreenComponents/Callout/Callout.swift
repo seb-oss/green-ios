83e04f08be36b0b87cb80a44555439fda69e0e14
Jacob Jidell <jacob.jidell@gmail.com>
2026-04-02 10:54:36 +0200

    refactor(Callout): Moved Actions inside the Callout.Model

--- Sources/SebGreenComponents/Callout/Callout.swift
+++ Sources/SebGreenComponents/Callout/Callout.swift
@@ -6,22 +6,17 @@ import SwiftUI
 
 public struct Callout: View {
     private let model: Model
-    private let actions: Actions
 
-    public init(
-        model: Model,
-        actions: Actions = .init()
-    ) {
+    public init(model: Model) {
         self.model = model
-        self.actions = actions
     }
 
     public var body: some View {
         GroupBox(model.title) {
             VStack(alignment: .leading, spacing: .space2xs) {
-                Text(model.message)
+                Text(model.shortText)
 
-                if let callToAction = actions.callToAction {
+                if let callToAction = model.actions.callToAction {
                     GreenButton(
                         title: callToAction.title,
                         kind: .tertiary,
@@ -39,7 +34,7 @@ public struct Callout: View {
         .groupBoxStyle(
             .callout(
                 variant: model.variant,
-                onClose: actions.onClose
+                onClose: model.actions.onClose
             )
         )
     }
@@ -53,19 +48,28 @@ extension Callout {
         case error
     }
 
-    public struct Model: Equatable {
+    public struct Model: Identifiable {
+        public let id: String
         public let title: String
-        public let message: String
+        public let shortText: String
+        public let message: String?
         public let variant: Variant
+        public let actions: Actions
 
         public init(
+            id: String,
             title: String,
-            message: String,
-            variant: Variant
+            shortText: String,
+            message: String? = nil,
+            variant: Variant,
+            actions: Actions = .init()
         ) {
+            self.id = id
             self.title = title
+            self.shortText = shortText
             self.message = message
             self.variant = variant
+            self.actions = actions
         }
     }
 
@@ -118,54 +122,58 @@ extension Callout {
         VStack(alignment: .leading, spacing: .spaceM) {
             Callout(
                 model: .init(
+                    id: "1",
                     title: "Information",
-                    message:
+                    shortText:
                         "Used for passive, non-critical updates like tips or background information.",
-                    variant: .information
+                    variant: .information,
+                    actions: .init(onClose: {})
                 ),
-                actions: .init(onClose: {})
             )
             Callout(
                 model: .init(
+                    id: "2",
                     title: "Notice",
-                    message:
+                    shortText:
                         "Used for actionable, attention-worthy updates that are still non-critical.",
-                    variant: .notice
-                ),
-                actions: .init(
-                    onClose: {},
-                    callToAction: .init(
-                        title: "See details",
-                        linkStyle: .internalLink,
-                        action: {}
+                    variant: .notice,
+                    actions: .init(
+                        onClose: {},
+                        callToAction: .init(
+                            title: "See details",
+                            linkStyle: .internalLink,
+                            action: {}
+                        )
                     )
-                )
+                ),
             )
 
             Callout(
                 model: .init(
+                    id: "3",
                     title: "Warning",
-                    message:
+                    shortText:
                         "Used to highlight important risks or information related to surrounding context.",
-                    variant: .warning
-                ),
-                actions: .init(
-                    callToAction: .init(
-                        title: "Read more",
-                        linkStyle: .externalLink,
-                        action: {}
+                    variant: .warning,
+                    actions: .init(
+                        callToAction: .init(
+                            title: "Read more",
+                            linkStyle: .externalLink,
+                            action: {}
+                        )
                     )
-                )
+                ),
             )
 
             Callout(
                 model: .init(
+                    id: "4",
                     title: "Error",
-                    message:
+                    shortText:
                         "Used to communicate that something has gone wrong or does not work and needs user attention.",
-                    variant: .error
+                    variant: .error,
+                    actions: .init(onClose: {})
                 ),
-                actions: .init(onClose: {})
             )
         }
         .padding(.spaceM)
