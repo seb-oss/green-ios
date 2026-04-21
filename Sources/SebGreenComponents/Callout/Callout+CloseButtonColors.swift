0e59fc8b2cb622c6d34687f41014c55653ea3b3d
Jacob Jidell <jacob.jidell@gmail.com>
2026-04-21 14:06:09 +0200

    updated close-button colors for information loud

--- Sources/SebGreenComponents/Callout/Callout+CloseButtonColors.swift
+++ Sources/SebGreenComponents/Callout/Callout+CloseButtonColors.swift
@@ -8,21 +8,13 @@ extension Callout.Variant {
 
     func closeButtonColors(surface: Surface) -> CloseButtonColors {
         switch self {
-        case .information(let style):
-            switch style {
-            case .subtle:
-                CloseButtonColors(
-                    primary: .contentNeutral02,
-                    secondary:
-                        surface == .neutral01 ? .l3Neutral01 : .l3Neutral02
-                )
-            case .loud:
-                CloseButtonColors(
-                    primary: .contentInversed,
-                    secondary: .white.opacity(0.16)  // TODO: Should be L3/netrual-tone (aka white: #FFFFFF)
-                )
-            }
-        case .notice, .warning, .critical:
+        case .information(.subtle):
+            CloseButtonColors(
+                primary: .contentNeutral02,
+                secondary:
+                    surface == .neutral01 ? .l3Neutral01 : .l3Neutral02
+            )
+        case .information(.loud), .notice, .warning, .critical:
             CloseButtonColors(
                 primary: .white,  // TODO: Should be contentNeutral05
                 secondary: .white.opacity(0.12)
