03b28f4b484a432b16d9825bd7cb945ea8459ca9
Herman Olsson <herman.olsson@framna.com>
2026-05-22 14:27:39 +0200

    Added GdsColor extensions for tonal colors not yet in GdsKit
    Defines l3Tonal01 and stateTonal01 as local GdsColor extensions,
    backed by the AdditionalGdsColours asset catalog, until these tokens
    are added upstream.

---
+++ Sources/SebGreenComponents/Extensions/CdsColours+AdditionalGdsColours.swift
@@ -0,0 +1,9 @@
+import GdsKit
+
+/*
+ If these colors collide with colors provided by GdsKit, then GdsKit always wins.
+ */
+extension GdsColor {
+    static let l3Tonal01 = GdsColor(rawValue: "l3Tonal01")
+    static let stateTonal01 = GdsColor(rawValue: "stateTonal01")
+}
